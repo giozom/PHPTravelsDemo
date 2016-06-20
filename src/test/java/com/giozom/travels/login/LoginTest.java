c8b5c2d8baf8d5dd60c6658244643f99e285df4c
giozom <garsenius@odecee.com.au>
2016-06-20 10:24:00 +1000

    Initial commit - new selenium demo

---
+++ src/test/java/com/giozom/travels/login/LoginTest.java
@@ -0,0 +1,28 @@
+package com.giozom.travels.login;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by garsenius on 20/06/2016.
+ */
+public class LoginTest {
+
+    @Test
+    public void loginToTravels(){
+
+        WebDriver driver = new FirefoxDriver();
+
+        //domain object for Travels application
+        TravelsAccount travels = new TravelsAccount();
+
+
+
+
+
+    }
+
+
+
+}
